f4bfa7ecf3c70a4c67740b06a440d85a272822c7
Vinicius-Rubia <vinicius.deoliveirarubia@gmail.com>
2025-12-10 23:15:47 -0300

    adicionar estilos

--- src/components/drawing-of-the-gallows.tsx
+++ src/components/drawing-of-the-gallows.tsx
@@ -6,11 +6,11 @@ interface DrawingOfTheGallowsProps {
 
 export function DrawingOfTheGallows({ mistakes }: DrawingOfTheGallowsProps) {
   return (
-    <div className="relative w-64 h-64 mb-8 bg-slate-100 rounded-lg flex justify-center items-center border border-slate-200">
-      <div className="absolute bottom-4 w-32 h-2 bg-slate-800 rounded" />
-      <div className="absolute bottom-4 left-1/2 -translate-x-12 w-2 h-56 bg-slate-800 rounded" />
-      <div className="absolute top-8 left-1/2 -translate-x-12 w-32 h-2 bg-slate-800 rounded" />
-      <div className="absolute top-8 right-16 w-2 h-8 bg-slate-800 rounded" />
+    <div className="relative w-64 h-64 mb-8 flex justify-center items-center">
+      <div className="absolute bottom-4 w-32 h-2 bg-white rounded" />
+      <div className="absolute bottom-4 left-1/2 -translate-x-12 w-2 h-56 bg-white rounded" />
+      <div className="absolute top-8 left-1/2 -translate-x-12 w-32 h-2 bg-white rounded" />
+      <div className="absolute top-8 right-16 w-2 h-8 bg-white rounded" />
       <BodyParts mistakes={mistakes} />
     </div>
   );
